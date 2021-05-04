796a742c1d0458e0301fabb2a728139e25ca4c9f
holdenlw <46573014+holdenlw@users.noreply.github.com>
2021-05-03 22:41:05 -0400

    Helper class to store data to file

--- app/src/main/java/com/example/myapplication/StoreInfo.java
+++ app/src/main/java/com/example/myapplication/StoreInfo.java
@@ -1,7 +1,24 @@
 package com.example.myapplication;
 
 
+import android.content.Context;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 public class StoreInfo {
@@ -9,19 +26,71 @@ public class StoreInfo {
     private HashMap<String, ArrayList<String>> data;
     private String id;
 
-    public StoreInfo(String id) {
-        // id = Android Device ID + YYYY/MM
-        this.id = id;
-        // if txt file already exists
-        // data[keys] = txt[keys]
-        // else
-        // data = {
-        //  key: []
-        //  create new txt file with ID as name with data as the content
+    public StoreInfo(Context context, String id, String cords, String alt, String speed, String address, String accuracy) {
+        // id = Android Device ID + time
+        this.id = id + Calendar.getInstance().toString();
+        this.data = new HashMap<String, ArrayList<String>>();
+
+        // when obj is created give the current values
+        this.data.put("Coordinates", new ArrayList<String>()).add(cords);
+        this.data.put("Altitude", new ArrayList<String>()).add(alt);
+        this.data.put("Speed", new ArrayList<String>()).add(speed);
+        this.data.put("Address", new ArrayList<String>()).add(address);
+        this.data.put("Confidence", new ArrayList<String>()).add(accuracy);
+
     }
 
-    // setters for each key of data
+    // writing file and putting it into local storage
+    public void writeFile(Context context) {
+        try  {
+            FileOutputStream fos = context.openFileOutput(id, Context.MODE_PRIVATE);
+            fos.write(data.toString().getBytes());
+        } catch (IOException e) {
+            Toast.makeText(context, "Can't write file" ,Toast.LENGTH_SHORT).show();
+        }
+    }
 
-    // option: either append to file each setter call, or append all changes at one time
+    public void updateData(String cords, String alt, String speed, String address, String accuracy) {
+        data.get("Coordinates").add(cords);
+        data.get("Altitude").add(alt);
+        data.get("Speed").add(speed);
+        data.get("Address").add(address);
+        data.get("Confidence").add(accuracy);
+    }
+
+
+    /* I will not use this function for now, but it will be useful in the future */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void updateFile(Context context)  {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            FileInputStream fis = context.openFileInput(id + ".txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                // going crazy with if statements
+                if (line.contains("Coordinates")) {
+                    stringBuilder.append(line).append(", ").append(data.get("Coordinates")).append('\n');
+                } else if (line.contains("Altitude")) {
+                    stringBuilder.append(line).append(", ").append(data.get("Altitude")).append('\n');
+                } else if (line.contains("Speed")) {
+                    stringBuilder.append(line).append(", ").append(data.get("Speed")).append('\n');
+                } else if (line.contains("Address")) {
+                    stringBuilder.append(line).append(", ").append(data.get("Address")).append('\n');
+                } else {
+                    stringBuilder.append(line).append(", ").append(data.get("Confidence")).append('\n');
+                }
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            Toast.makeText(context, "Can't find file", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(context, "idk", Toast.LENGTH_SHORT).show();
+        } finally {
+            String contents = stringBuilder.toString();
+        }
+        // I just realized I could just write the file once at the end...
+    }
 
 }
